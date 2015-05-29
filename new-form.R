e88ac5a252ce65c22df540f13a2e3deda940f51a
seregastl <sergey@sergeev.co.uk>
2015-05-29 18:13:49 +0300

    01

---
+++ new-form.R
@@ -0,0 +1,6 @@
+all_data <- read.csv('C:/Users/sergey.sergeev/Documents/!/out-2015-05-28.csv', sep=";", header=TRUE)
+
+
+
+all_providers <- read.csv('C:/Users/sergey.sergeev/Documents/!/Аналитика/По дате выезда/all_providers.csv', sep=";", header=TRUE)
+all_providers <- all_providers[1:7]
