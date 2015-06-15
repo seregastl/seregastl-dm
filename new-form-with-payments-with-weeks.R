3c75e2f5527d93461137f881742c7506f93b209c
seregastl <sergey@sergeev.co.uk>
2015-06-15 13:10:06 +0300

    2015-06-15 1

--- new-form-with-payments-with-weeks.R
+++ new-form-with-payments-with-weeks.R
@@ -2,7 +2,8 @@ library(dplyr)
 require(dplyr)
 library(ggplot2)
 
-all_data <- read.csv('C:/Users/sergey.sergeev/Documents/!/out-2015-06-11-14.csv', sep=";", header=TRUE)
+all_data <- read.csv('C:/Users/sergey.sergeev/Documents/!/nf-2014-росси.csv', sep=";", header=TRUE)
+#all_data <- mutate(all_data, book_day = gsub("\\s.*", "", Дата.бронирования))
 all_data <- mutate(all_data, book_day = as.Date(as.character(gsub("\\s.*", "", Дата.бронирования)), '%d.%m.%Y'))
 
 all_data <- mutate(all_data, id1 = gsub("(\\d+)-\\d+\\s*$", "!\\1", X..брони))
@@ -14,7 +15,7 @@ all_data <- mutate(all_data, Week = as.numeric(W1) + 1)
 #all_data <- filter(all_data, as.POSIXct(book_day) >= as.POSIXct("2015-06-01"))
 #all_data <- filter(all_data, as.POSIXct(book_day) >= as.POSIXct("2015-05-25")&as.POSIXct(book_day) < as.POSIXct("2015-06-01"))
 
-
+all_data <- filter(all_data, ID != 11)
 all_data <- filter(all_data, !grepl("Отмена", Статус.брони))
 
 all_data <- mutate(all_data, Способ.оплаты = gsub("\\s\\[.*", "", Способ.оплаты))
@@ -27,12 +28,12 @@ all_data <- mutate(all_data, beznal2 = ifelse(Способ.оплаты == "Ба
 all_data <- mutate(all_data, other = ifelse(Способ.оплаты != "При заселении" & Способ.оплаты != "Банк. карта" & Способ.оплаты != "Безнал для юр.лиц" & Способ.оплаты != "Банковский перевод для физ.лиц", 1, 0))
 
 
-ids <- group_by(all_data, ID, book_day)
+#ids <- group_by(all_data, ID, book_day)
 #ids <- group_by(all_data, ID)
 #ids <- group_by(all_data, Week)
-#ids <- group_by(all_data, ID, Week)
+ids <- group_by(all_data, ID, Week)
 (per_day <- summarise(ids, bookings = n(), costs = sum(Стоимость..руб.), nights = sum(Ночей), guests = sum(Гостей), rooms = sum(Номеров)
-#                      , Гостиниц = n_distinct(ID)
+                      , Гостиниц = n_distinct(ID)
                       , При.заселении = sum(checkin)
                       , banks = sum(banks)
                       , Безнал.для.юр.лиц = sum(beznal1)
@@ -58,6 +59,9 @@ per_day$costs <- format(round(per_day$costs, 2), nsmall=2, big.mark=" ")
 #   theme(plot.title = element_text(size=14, family="Trebuchet MS", face="bold", hjust=0, color="#666666"))
 # 
 # ggsave("hotels.png", width=14, height=8, p)
+
+
+
 #per_day <- per_day %>% select(Week:costs, avg_cost, nights:rooms, Гостиниц:Другие)
 #colnames(per_day) <- c("Неделя", "Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров","Гостиниц", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие")
 #con <- file("res.csv",encoding="UTF-8")
@@ -71,6 +75,9 @@ all_providers <- all_providers[1:7]
 merged_table <- merge(x = per_day, y = all_providers,by="ID",all.x=TRUE)
 
 merged_table <- filter(merged_table, status!= 0)
+
+#merged_table <- mutate(merged_table, avg_cost = costs/bookings)
+
 
 #ga_data <- read.csv('C:/Users/sergey.sergeev/Documents/!/nf-2015-06-01-06-ga.csv', sep=";", header=TRUE)
 #ga_data <- read.csv('C:/Users/sergey.sergeev/Documents/!/nf-2015-05-25-31-ga.csv', sep=";", header=TRUE)
@@ -88,15 +95,21 @@ merged_table <- filter(merged_table, status!= 0)
 
 merged_table <- merged_table %>% select(ID:costs, avg_cost, nights:rooms, При.заселении:Другие, Гостиница:Регион)
 
+
+
+#merged_table$avg_cost <- format(round(merged_table$avg_cost, 2), nsmall=2, big.mark=" ")
+#merged_table$costs <- format(round(merged_table$costs, 2), nsmall=2, big.mark=" ")
+#merged_table$conversion <- format(round(merged_table$conversion, 2), nsmall=2, big.mark=" ")
+
 # merged_table %>%
 #   ggplot(aes(x=book_day, y=bookings, group=1)) + 
 #   geom_bar(stat="identity") +
 #   facet_wrap(~ ID)  
 
 #colnames(merged_table) <- c("ID","Броней","Стоимость броней","Ср чек","Визиты","Конверсия","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
-colnames(merged_table) <- c("ID","Дата бронирования","Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
+#colnames(merged_table) <- c("ID","Дата бронирования","Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
 #colnames(merged_table) <- c("ID","Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
-#colnames(merged_table) <- c("ID","Неделя", "Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
+colnames(merged_table) <- c("ID","Неделя", "Броней","Стоимость броней","Ср чек","Ночей","Гостей","Номеров", "При заселении", "Банк карта", "Безнал юр лица", "Безнал физ лица", "Другие", "Гостиница","Город","Сайт","Страна","Регион")
 
 con <- file("res.csv",encoding="UTF-8")
 write.csv(merged_table,con,row.names=FALSE)
